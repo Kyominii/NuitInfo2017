f7dd1a4369bf3346c524d6043d8c32cb795cbf4e
Teddy VALETTE <teddy.valette@telecomnancy.eu>
2017-12-06 11:37:28 +0100

    Changement de la méthode de déploiement

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ app.get('/', (req, res) => {
   res.send('<h1>another hello world</h1>');
 });
 
+app.get('/deploy', (req, res) => {
+  var child = exec("cd ~/www ; git pull", function(err, stdout, stderr){
+        if(err != null){
+            res.send('<h1>Error during deployment !</h1><p>' . stderr . '</p>');
+        }else{
+            res.send('<h1>Deployment completed !</h1><p>' . stdout . '</p>');
+        }
+    });
+});
+
 app.listen(4546);
