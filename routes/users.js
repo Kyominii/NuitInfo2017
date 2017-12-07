606b19b8db33d90f246998bfc5cd69b96e9e0060
Teddy VALETTE <teddy.valette@telecomnancy.eu>
2017-12-08 00:50:59 +0100

    add vehicles list

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,15 @@ var mysql = require('../accessBDD');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   if (req.session.pseudo) {
-    res.render('users', {displayMode: 0, pseudo: req.session.pseudo});
+    var query = "SELECT * FROM cars WHERE user_id=" + req.session.userid;
+    mysql.query(query, (err, result) => {
+      if(err) {
+        throw err;
+      }
+      else {
+        res.render('users', {displayMode: 0, pseudo: req.session.pseudo, data: result});
+      }
+    });
   } else {
     res.send('Not connected !')
   }
