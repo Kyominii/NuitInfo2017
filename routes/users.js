091d38b125eb6b92fe2ad4897618879f31b9237a
Teddy VALETTE <teddy.valette@telecomnancy.eu>
2017-12-07 22:52:07 +0100

    test session for connected users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (req.session.user) {
+    res.send('Welcome back : ' + req.session.user);
+  } else {
+    res.send('Not connected !')
+  }
+});
+
+router.get('/car/add', function(req, res, next) {
+
 });
 
 module.exports = router;
