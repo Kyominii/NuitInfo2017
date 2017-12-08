d3a67b4205ba352f5b40a9eb7c69f18495a882a8
Teddy VALETTE <teddy.valette@telecomnancy.eu>
2017-12-08 04:04:11 +0100

    shitty fix for 404 page

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(function(err, req, res, next) {
   } else {
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error404');
   }
 });
 
