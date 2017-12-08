aa14dedd934e82a556809571db202c14673396e8
Teddy VALETTE <teddy.valette@telecomnancy.eu>
2017-12-08 04:34:25 +0100

    again

--- app.js
+++ app.js
@@ -56,24 +56,6 @@ app.use('/login', login);
 app.use('/signin', signin);
 app.use('/logout', logout);
 app.use('/tram', tram);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 app.use('/neighbor',neighbor);
 app.use('/findsam', findsam);
 app.use('/findcar', findcar);
