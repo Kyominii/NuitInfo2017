1b0e28eb95d16ddbbafc879fd65b394e619eea51
ValentinDebenathTncy <valentin.debenath@gmail.com>
2017-12-08 02:45:21 +0100

    jsp

--- routes/findcar.js
+++ routes/findcar.js
@@ -31,7 +31,7 @@ router.post('/', function (req, res, next) {
                     if(err) throw err;
                     var sql3 = 'SELECT * FROM cars WHERE user_id=' + req.session.userid +' AND available=0';
                     con.query(sql3, function (err, result) {
-                        res.render('findcar', { cars:result, carAdd:true});
+                        res.render('findcar', { cars:result, carAdd:true, title:'Devenir Sam'});
                     })
                 })
             })
@@ -48,7 +48,7 @@ router.get('/',function (req,res) {
         res.setHeader('Content-Type', 'text/html');
         var sql = 'SELECT * FROM cars WHERE user_id=' + req.session.userid +' AND available=0';
         con.query(sql, function (err, result) {
-            res.render('findcar', { cars:result, carAdd:false});
+            res.render('findcar', { cars:result, carAdd:false, title:'Devenir Sam'});
         })
     }
     else
--- routes/neighbor.js
+++ routes/neighbor.js
@@ -2,7 +2,8 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/',function(req,res){
-    res.render('user', { title: 'Samyste in Nancy ', subtitle: 'Have a look on the distance of your home and your samyste' });
+    console.log(req.session);
+    res.render('user', { title: 'Samyste in Nancy ', subtitle: 'Have a look on the distance of your home and your samyste' , session:req.session });
 });
 
 module.exports = router;
